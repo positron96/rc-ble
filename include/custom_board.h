29ca22148c87d49d03685c5508eb534808faf6f3
Paul Melnikov <paul.melnikov@imec.nl>
2024-09-02 15:09:52 +0200

    Compiled ble_app_blinky

--- include/custom_board.h
+++ include/custom_board.h
@@ -8,17 +8,23 @@ extern "C" {
 #include "nrf_gpio.h"
 
 // LEDs definitions for PCA10040
-#define LEDS_NUMBER    1
+#define LEDS_NUMBER    4
 
-#define LED_1          14
+#define LED_1     5
+#define LED_2     6
+#define LED_3     9
+#define LED_4     15
 
 #define LEDS_ACTIVE_STATE  1
 
 #define LEDS_INV_MASK  LEDS_MASK
 
-#define LEDS_LIST { LED_1 }
+#define LEDS_LIST { LED_1, LED_2, LED_3, LED_4}
 
 #define BSP_LED_0      LED_1
+#define BSP_LED_1      LED_2
+#define BSP_LED_2      LED_3
+#define BSP_LED_3      LED_4
 
 #define BUTTONS_NUMBER 0
 
