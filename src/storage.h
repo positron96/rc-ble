0c171569dfa4cc99d62a61fae47173c5c80d5716
Paul Melnikov <positron96@gmail.com>
2025-03-19 00:06:20 +0100

    Change default name

--- src/storage.h
+++ src/storage.h
@@ -9,7 +9,7 @@ namespace storage {
     void init();
 
     constexpr size_t DEVNAME_LEN = 64;
-    constexpr char DEFAULT_DEVNAME[] = "\xF0\x9F\x9A\x9A\xd0\x9a\xd0\xb0\xd0\xbc\xd0\xb0\xd0\xb7 Truck";// "NanoRC";
+    constexpr char DEFAULT_DEVNAME[] = "MicroRC\xF0\x9F\x9A\x9A";// "NanoRC";
     etl::optional<etl::string_view> get_dev_name();
     bool set_dev_name(const etl::string_view name);
 
